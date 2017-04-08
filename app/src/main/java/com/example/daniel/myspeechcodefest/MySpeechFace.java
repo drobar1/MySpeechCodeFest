5bb2b97372a95af57ef06a36fa8f413f82845708
Daniel Robar <dwr8@students.uwf.edu>
2017-04-08 08:46:15 -0500

    Initial commit

---
+++ app/src/main/java/com/example/daniel/myspeechcodefest/MySpeechFace.java
@@ -0,0 +1,13 @@
+package com.example.daniel.myspeechcodefest;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MySpeechFace extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_my_speech_face);
+    }
+}
