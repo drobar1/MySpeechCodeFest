7ced39d9312975e9776abfa1202ef6fe1a7da863
Jakobi <jgs14@students.uwf.edu>
2017-04-08 10:30:20 -0500

    Added button functionality to change between scenes

--- app/src/main/java/com/example/daniel/myspeechcodefest/LoginActivity.java
+++ app/src/main/java/com/example/daniel/myspeechcodefest/LoginActivity.java
@@ -2,6 +2,10 @@ package com.example.daniel.myspeechcodefest;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.content.Intent;
+import android.view.View;
+import android.widget.Button;
+
 
 public class LoginActivity extends AppCompatActivity {
 
@@ -9,5 +13,15 @@ public class LoginActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+
+        Button advanceToRegistrationPage = (Button) findViewById(R.id.RegisterButton);
+        advanceToRegistrationPage.setOnClickListener(new View.OnClickListener(){
+
+            @Override
+            public void onClick(View view){
+                Intent intent = new Intent(LoginActivity.this,RegisterActivity.class);
+                startActivity(intent);
+            }
+        });
     }
 }
