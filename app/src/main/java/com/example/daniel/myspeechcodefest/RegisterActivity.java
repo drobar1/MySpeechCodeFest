885c3e9eeace1370234598c734d79a890b23af69
Jakobi <jgs14@students.uwf.edu>
2017-04-08 14:36:24 -0500

    Added button functionality from register back to login and to selection

--- app/src/main/java/com/example/daniel/myspeechcodefest/RegisterActivity.java
+++ app/src/main/java/com/example/daniel/myspeechcodefest/RegisterActivity.java
@@ -1,7 +1,10 @@
 package com.example.daniel.myspeechcodefest;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 import android.widget.TextView;
 
 public class RegisterActivity extends AppCompatActivity {
@@ -10,5 +13,24 @@ public class RegisterActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_register);
+
+        Button registerToLogin = (Button) findViewById(R.id.RegisterToLogin);
+        registerToLogin.setOnClickListener(new View.OnClickListener(){
+
+            @Override
+            public void onClick(View view){
+                Intent intent = new Intent(RegisterActivity.this,LoginActivity.class);
+                startActivity(intent);
+            }
+        });
+        Button buttonRegisterToSelection = (Button) findViewById(R.id.ButtonToSelection);
+        buttonRegisterToSelection.setOnClickListener(new View.OnClickListener(){
+
+            @Override
+            public void onClick(View view){
+                Intent intent = new Intent(RegisterActivity.this,SelectionScreen.class);
+                startActivity(intent);
+            }
+        });
     }
 }
