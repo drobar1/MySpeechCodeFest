4e74365f327f9faba4dccaf84028c182edbca855
Jakobi <jgs14@students.uwf.edu>
2017-04-09 08:49:11 -0500

    Fixed the selection screen buttons

--- app/src/main/java/com/example/daniel/myspeechcodefest/LoginActivity.java
+++ app/src/main/java/com/example/daniel/myspeechcodefest/LoginActivity.java
@@ -8,7 +8,7 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
 
-
+//For the love of god commit
 public class LoginActivity extends AppCompatActivity
 {
 
--- app/src/main/java/com/example/daniel/myspeechcodefest/SelectionScreen.java
+++ app/src/main/java/com/example/daniel/myspeechcodefest/SelectionScreen.java
@@ -10,7 +10,7 @@ import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
 import android.widget.Button;
-
+//Commit please
 public class SelectionScreen extends AppCompatActivity {
 
     Typeface tf3;
